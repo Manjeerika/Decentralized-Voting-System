a53ea8fff6a18e0f152c6c1ee8906d4b0d4d0cc0
Manjeerika <kmanjeerika@gmail.com>
2025-06-20 01:11:44 +0530

    Create Decentralized Voting System.sol

---
+++ Contracts/Decentralized Voting System.sol
@@ -0,0 +1,25 @@
+{
+  "name": "decentralized-voting-system",
+  "version": "1.0.0",
+  "description": "A blockchain-based transparent voting system",
+  "main": "index.js",
+  "scripts": {
+    "compile": "npx hardhat compile",
+    "test": "npx hardhat test",
+    "deploy": "npx hardhat run scripts/deploy.js"
+  },
+  "keywords": [
+    "blockchain",
+    "voting",
+    "ethereum",
+    "solidity",
+    "smart-contracts",
+    "decentralized"
+  ],
+  "author": "Your Name",
+  "license": "MIT",
+  "devDependencies": {
+    "@nomicfoundation/hardhat-toolbox": "^3.0.0",
+    "hardhat": "^2.17.1"
+  }
+}
